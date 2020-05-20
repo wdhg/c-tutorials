896d4b1222bb004a3c743ca16161b01d74146e18
wdhg <wdhgrant@gmail.com>
2020-05-20 16:16:06 +0100

    add createCommands

--- week2/pointers_and_dynamic_memory.c
+++ week2/pointers_and_dynamic_memory.c
@@ -4,4 +4,10 @@ char *createCommand(size_t size) {
   return str;
 }
 
+char **createCommands(int n) {
+  char **ptrs;
+  ptrs = (char **)calloc((size_t)sizeof(char *) * n);
+  ptrs = (char **)
+}
+
 int main(int argc, char *argv[]) { return 0; }
