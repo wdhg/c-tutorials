3d671cb67fed978ae2a7fe0e8779e44ba5fc1255
wdhg <wdhgrant@gmail.com>
2020-05-20 16:23:29 +0100

    fix errors

--- week2/pointers_and_dynamic_memory.c
+++ week2/pointers_and_dynamic_memory.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 char *createCommand(size_t size) {
   char *str;
-  str = (char *)calloc(size);
+  str = (char *)calloc(size, sizeof(char));
   return str;
 }
 
 char **createCommands(int n) {
   char **ptrs;
-  ptrs = (char **)calloc((size_t)sizeof(char *) * n);
-  ptrs = (char **)
+  ptrs = (char **)calloc((size_t)n, sizeof(char *));
+  return ptrs;
 }
 
 char *getCommand(size_t size) {
