cd12d0744e588beb7cbe675c5d929fab2fc40bcc
wdhg <wdhgrant@gmail.com>
2020-05-20 16:19:22 +0100

    add getCommand

--- week2/pointers_and_dynamic_memory.c
+++ week2/pointers_and_dynamic_memory.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 char *createCommand(size_t size) {
   char *str;
   str = (char *)calloc(size);
@@ -8,6 +10,12 @@ char **createCommands(int n) {
   char **ptrs;
   ptrs = (char **)calloc((size_t)sizeof(char *) * n);
   ptrs = (char **)
+}
+
+char *getCommand(size_t size) {
+  char *command = createCommand(size);
+  printf("> ");
+  fgets(command, size, stdin);
 }
 
 int main(int argc, char *argv[]) { return 0; }
