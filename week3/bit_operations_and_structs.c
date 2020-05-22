776588a36d1fff274f7962de452cfd34910d1a5e
wdhg <wdhgrant@gmail.com>
2020-05-22 10:57:31 +0100

    add printBits16

--- week3/bit_operations_and_structs.c
+++ week3/bit_operations_and_structs.c
@@ -15,6 +15,14 @@ void printBits(uint32_t x) {
   printf("\n");
 }
 
+void printBits16(uint16_t x) {
+  uint16_t mask = 1 << 15;
+  for (int i = 0; i < 16; ++i) {
+    printf(x & mask ? "1" : "0");
+    mask = mask >> 1;
+  }
+}
+
 void splitBits(uint32_t x, uint16_t *first, uint16_t *second) {
   *first = x >> 16;
   *second = 0x0000ffff & x;
@@ -24,5 +32,9 @@ int main(int argc, char *argv[]) {
   printBits(N);
   uint16_t first, second;
   splitBits(N, &first, &second);
+  printBits16(first);
+  printf(" ");
+  printBits16(second);
+  printf("\n");
   return 0;
 }
