9557a3e72fee5df818028e13a906b870c085defa
wdhg <wdhgrant@gmail.com>
2020-06-06 13:43:36 +0100

    implement ann_create

--- ann/ann.c
+++ ann/ann.c
@@ -1,32 +1,36 @@
 #include "ann.h"
 
 /* Creates and returns a new ann. */
-ann_t *ann_create(int num_layers, int *layer_outputs)
-{
+ann_t *ann_create(int num_layers, int *layer_outputs) {
   /**** PART 2 - QUESTION 1 ****/
-  return NULL; // delete after implementing
+  ann_t *ann = (ann_t *)calloc(1, sizeof(ann_t));
+  ann->input_layer = layer_init(layer_create(), layer_outputs[0], NULL);
+  layer_t *prev_layer = ann->input_layer;
+  for (int i = 1; i < num_layers; i++) {
+    layer_t *layer = layer_init(layer_create(), layer_outputs[i], prev_layer);
+  }
+  ann->output_layer = prev_layer->next;
+  return ann;
   /* 4 MARKS */
 }
 
 /* Frees the space allocated to ann. */
-void ann_free(ann_t *ann)
-{
+void ann_free(ann_t *ann) {
   /**** PART 2 - QUESTION 2 ****/
 
   /* 2 MARKS */
 }
 
 /* Forward run of given ann with inputs. */
-void ann_predict(ann_t const *ann, double const *inputs)
-{
+void ann_predict(ann_t const *ann, double const *inputs) {
   /**** PART 2 - QUESTION 3 ****/
 
   /* 2 MARKS */
 }
 
 /* Trains the ann with single backprop update. */
-void ann_train(ann_t const *ann, double const *inputs, double const *targets, double l_rate)
-{
+void ann_train(ann_t const *ann, double const *inputs, double const *targets,
+               double l_rate) {
   /* Sanity checks. */
   assert(ann != NULL);
   assert(inputs != NULL);
